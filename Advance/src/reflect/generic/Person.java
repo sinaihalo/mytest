ce4bcf55d0de91b9f11151919628e7f49c567f2e
sh <haoxinkai@ultrapower.com.cn>
2018-11-20 10:45:28 +0800

    泛型和反射

---
+++ Advance/src/reflect/generic/Person.java
@@ -0,0 +1,5 @@
+package reflect.generic;
+
+public class Person<T> {
+
+}
---
+++ Advance/src/reflect/generic/Student.java
@@ -0,0 +1,23 @@
+package reflect.generic;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+public class Student extends Person<Student>{
+
+public static void main(String[] args) {
+		Student st=new Student();
+		Class clazz=st.getClass();
+		//getSuperclass()获得该类的父类
+		System.out.println(clazz.getSuperclass());
+		//getGenericSuperclass()获得带有泛型的父类
+		//Type是 Java 编程语言中所有类型的公共高级接口。它们包括原始类型、参数化类型、数组类型、类型变量和基本类型。
+		Type type=clazz.getGenericSuperclass();
+		System.out.println(type);
+		//ParameterizedType参数化类型，即泛型
+		ParameterizedType p=(ParameterizedType)type;
+		//getActualTypeArguments获取参数化类型的数组，泛型可能有多个
+		Class c=(Class) p.getActualTypeArguments()[0];
+		System.out.println(c);
+	}
+}
