fb3447127e24523837f7e35960f28eb567c5d80c
sh <haoxinkai@ultrapower.com.cn>
2019-03-26 14:58:22 +0800

    extends

---
+++ Advance/src/interview/HelloA.java
@@ -0,0 +1,9 @@
+package interview;
+
+public class HelloA {
+	public HelloA(){
+		System.out.println("HelloA");
+	}
+	static{System.out.println("static A");}
+	{System.out.println("I am A class");}
+}
---
+++ Advance/src/interview/HelloB.java
@@ -0,0 +1,27 @@
+package interview;
+/**
+	static A
+	static B
+	I am A class
+	HelloA
+	I am B class
+	HelloB
+	
+	1.静态块，加载类
+ 	2.{}初始化块，创建对象
+ 	3.构造器
+ 	4.先父类后子类
+ *
+ */
+public class HelloB extends HelloA{
+	public HelloB(){
+		System.out.println("HelloB");
+	}
+	{System.out.println("I am B class");}
+	static{System.out.println("static B");}
+	public static void main(String[] args) {
+		new HelloB();
+
+	}
+
+}
