eb43949e19452740ec8ec941877555d03a86d224
sh <haoxinkai@ultrapower.com.cn>
2020-04-02 14:33:39 +0800

    jpython

--- Advance/src/jpython/TestPython.java
+++ Advance/src/jpython/TestPython.java
@@ -9,18 +9,18 @@ public class TestPython {
 		 Properties props = new Properties();
 		 props.put("python.home", "C:\\Users\\dell\\Desktop\\jars\\Lib");
 		 props.put("python.console.encoding", "UTF-8");        
-		         props.put("python.security.respectJavaAccessibility", "false");        
+		 props.put("python.security.respectJavaAccessibility", "false");        
 		 props.put("python.import.site", "false");
 
 		 Properties preprops = System.getProperties();
 		 PythonInterpreter.initialize(preprops, props, new String[0]);
 		 
-	        PythonInterpreter interpreter = new PythonInterpreter();
-	        String script = new String();
-	        script+="import sys  ;";
-	        script+=" a = 2 ;";
-	        script+=" b = 3 ;";
-	        script+=" print('%s * %s = %s' %(a, b, a*b))";
-	        interpreter.exec(script);
+        PythonInterpreter interpreter = new PythonInterpreter();
+        String script = new String();
+        script+="import sys  ;";
+        script+=" a = input('Enter a:') ;";
+        script+=" b = input('Enter b:') ;";
+        script+=" print('%s * %s = %s' %(a, b, a*b))";
+        interpreter.exec(script);
 	    }
 }
