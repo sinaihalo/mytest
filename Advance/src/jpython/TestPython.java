415bf878e503ec1007835ad1d595817835ed91af
sh <haoxinkai@ultrapower.com.cn>
2020-04-02 15:09:42 +0800

    jpython

--- Advance/src/jpython/TestPython.java
+++ Advance/src/jpython/TestPython.java
@@ -3,10 +3,16 @@ package jpython;
 import java.util.Properties;
 
 import org.python.util.PythonInterpreter;
-
+/**
+ * @version: 
+* @Description:  导jar包：Jython  2.7.0
+* @author: hxk 
+* @date: 2020年4月2日下午2:41:37
+ */
 public class TestPython {
 	 public static void main(String args[]) {
 		 Properties props = new Properties();
+		 //Jython jar包目录
 		 props.put("python.home", "C:\\Users\\dell\\Desktop\\jars\\Lib");
 		 props.put("python.console.encoding", "UTF-8");        
 		 props.put("python.security.respectJavaAccessibility", "false");        
