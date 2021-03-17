6cc54773e279d4e3b15be4caf23dde2e5fd5780e
sh <haoxinkai@ultrapower.com.cn>
2021-03-17 17:30:24 +0800

    ==

--- Advance/src/interview/TypeConversion.java
+++ Advance/src/interview/TypeConversion.java
@@ -8,7 +8,7 @@ package interview;
  */
 public class TypeConversion {
     public static void main(String[] args) {
-
+        new TypeConversion().m6();
     }
     void m1 (){
         String s = "你好"; int i=3; s +=i;// stringbuilder append
@@ -25,5 +25,13 @@ public class TypeConversion {
     }
     void m5 (){
         String s = null; int i=(s!=null)&&(s.length())>0?s.length():0;
+    }
+    void m6(){
+        int a = 129;int b = 129;System.out.println(a==b);
+        /*
+         * 当==两边是对象时（String,Integer...），两边比的都是地址
+           当==两边是基本类型时(int,float)，两边比的都是值
+           默认equals比的是对象的地址，但是重写的话可以改变成比较值，String和Integer的equals就是重写过的
+        */
     }
 }
