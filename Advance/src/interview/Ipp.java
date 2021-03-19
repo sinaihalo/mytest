5a0c9e66114620a240ccffb2b97b31a8ae285756
sh <haoxinkai@ultrapower.com.cn>
2021-03-19 13:38:35 +0800

    笔试

--- Advance/src/interview/Ipp.java
+++ Advance/src/interview/Ipp.java
@@ -1,5 +1,9 @@
 package interview;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @version:
  * @Description:
@@ -15,5 +19,15 @@ public class Ipp {
     }
     public static void main(String[] args) {
         new Ipp().method();
+        new Ipp().method1();
+    }
+    void method1(){//方法：入参出参必有
+        int x = 1,y=2,z=3;
+        y+=z--/++x;
+        System.out.println(y);//3
+        List l = new LinkedList();
+        ((LinkedList) l).removeLast();
+        l = new ArrayList();
+
     }
 }
